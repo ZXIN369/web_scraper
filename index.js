a46083278099d9a109edc4d8fdfa8dd06a113f7a
Zilong Mindshape <92637829+ZXIN369@users.noreply.github.com>
2022-05-15 22:55:04 -0400

    add simple front page

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const PORT = 8000
 const URL = 'https://www.theguardian.com/uk'
+const TAG = 'a'
+const ATTRIBUTE = 'href'
 
+//app.METHOD(PATH, HANDLER)
+// app.get() // get data
+// app.post() // add data
+// app.put() // edit data
+// app.delete() // delete data
 
-axios(URL)
-    .then(res => {
-        const html = res.data
-        // console.log(html)
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.fc-item__title', html).each(function(){
-            const title = $(this).text()
-            const link = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                link
+app.use(cors())
+
+app.get('/', (req, res)=> {
+    res.json('This web-scraper is running')
+})
+
+app.get('/results', (req, res)=> {
+    axios(URL)
+        .then(response => {
+            const html = response.data
+            // console.log(html)
+            const $ = cheerio.load(html)
+            const articles = []
+            $('.fc-item__title', html).each(function(){
+                const title = $(this).text()
+                const url = $(this).find(TAG).attr(ATTRIBUTE)
+                articles.push({
+                    title,
+                    url
+                })
             })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+            // console.log(articles)
+            res.json(articles)
+        }).catch(err => console.log(err))
+})
+
 
 
 app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
